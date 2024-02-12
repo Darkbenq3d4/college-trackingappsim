abd8cee3be00b23f64891c7a8bcf9b52097fb033
David Rosario <darosario3d@gmail.com>
2024-02-11 22:11:54 -0400

    Refactoring WIP

--- System.py
+++ System.py
@@ -7,36 +7,25 @@
 
 import paho.mqtt.client as paho
 import paho.mqtt.publish as publish
-import time as Ti
+import time
+import threading
+import queue
+
 
 """
 Setting Messages and host
 """
-localtime = Ti.time()
-localtimeFormated = Ti.localtime(localtime)
+localtime = time.time()
+localtimeFormated = time.localtime(localtime)
 date = str(localtimeFormated.tm_year)+"-"+str(localtimeFormated.tm_mon)+"-"+str(localtimeFormated.tm_mday)+" "+str(localtimeFormated.tm_hour)+":"+str(localtimeFormated.tm_min)+":"+str(localtimeFormated.tm_sec)
 receiverTopic = "Sistema/Receiver"
 alertTopic = "Sistema/Alert"
-
-
 msgs = [{'topic': receiverTopic, 'payload':"45.252,-75.541,95,120,60,90, date: " + date},
         {'topic': receiverTopic, 'payload':"45.252,-73.343,85,110,60,90, date: " + date},
         {'topic': receiverTopic, 'payload':"45.252,-77.233,65,110,60,90, date: " + date},
         {'topic': receiverTopic, 'payload':"45.252,-65.543,53,109,60,90, date: " + date}]
-
 host = "localhost"
-
-def on_message(client, obj, msg):
-    if msg.payload.decode("utf-8") == "1" and msg.topic == alertTopic:
-        print ("Boton de Alerta Activado - ",date,sep = " ")
-        print ("A la espera de mensajes...")
-        publish.multiple(msgs, hostname = host)
-    elif msg.topic == alertTopic and msg.payload.decode("utf-8") == "2":
-        print ("Alerta desactivada, sensor pasa a standby")
-    else:
-        print(msg.payload.decode("utf-8"))
-
-
+Q = queue.Queue()
 
 def on_connect(client, userdata, flags, rc):
      print("Conexion exitosa al sistema, esperando comando de activacion...")
@@ -46,14 +35,43 @@ def on_connect(client, userdata, flags, rc):
           client.subscribe("Sistema/#")
           print("Conexion exitosa a Alertas")
 
+def main():
+        print("Espere...")
+        while True:
+             global restart 
+             restart = False
+             if restart == True:
+                  break
+             else:
+                time.sleep(1)
+                publish.multiple(hostname=host,msgs=msgs)
+                continue
+
+def subscriber():
+     client.on_message = on_message
+     client.loop_forever()
+
+def on_message(client, obj, msg):
+    if msg.payload.decode("utf-8") == "1" and msg.topic == alertTopic:
+        print ("Boton de Alerta Activado - ",date,sep = " ")
+        print ("A la espera de mensajes...")
+        restart = True
+    elif msg.topic == alertTopic and msg.payload.decode("utf-8") == "2":
+        print ("Alerta desactivada, sensor pasa a standby")
+        restart = False
+        #print(pub.is_alive(),sub.is_alive())
+    else:
+        print(msg.payload.decode("utf-8"))
+
 if __name__ == '__main__':
 
     client =  paho.Client()
     client.on_connect = on_connect
-    client.on_message = on_message
     client.connect(host, 1883, 60)
-    client.loop_forever()
-
+    sub = threading.Thread(target=subscriber,name="Sub")
+    pub = threading.Thread(target=main, name= "Pub")
+    sub.start()
+    pub.start()
 
 
     
